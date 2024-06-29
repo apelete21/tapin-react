c8497ff9daae6c11dc77bc6497ee03b5f086537f
Apelete <adzohonouapelete@gmail.com>
2024-06-29 07:34:36 -0700

    favicon updated

--- components/head/index.jsx
+++ components/head/index.jsx
@@ -7,6 +7,7 @@ export default function Header() {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+            <link rel="shortcut icon" href="/icon.png" type="image/x-icon" />
             <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet" />
             <title>Linkaard</title>
         </>
